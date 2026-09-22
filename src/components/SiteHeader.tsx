8bf567af2a569cac0f23cd0d592de2be85df8057
nexsourceglobal <info@nexsource.co>
2026-09-22 21:11:15 +0530

    Update SiteHeader.tsx

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -56,7 +56,7 @@ export function SiteHeader() {
   aria-label="Nex Source Global home"
 >
   <img
-    src="https://stimg.cardekho.com/images/uploadimages/1789898215458/WhatsApp-Image-2026-09-20-at-3.26.40-PM.jpeg"
+    src="https://i.ibb.co/MyKwWztQ/Nex-Source-Exact-Logo.jpg"
     alt="Nex Source Global"
     className="h-14 w-[200px] object-contain object-left"
   />
